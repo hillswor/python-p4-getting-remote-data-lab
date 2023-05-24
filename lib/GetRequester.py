7e5d1dc579f54cc92ece38860ac6e86e37754a6a
Bruce <bruce.hillsworth@gmail.com>
2023-05-24 14:54:32 -0600

    Pass tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,20 @@
 import requests
 import json
+import ipdb
+
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        people_list = []
+        people = json.loads(self.get_response_body())
+        for person in people:
+            people_list.append(person)
+
+        return people_list
